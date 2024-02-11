b1b1aa8715df78694e2613720e90fd51bb76259c
osama.abdulsattar <osama3030@gmail.com>
2024-02-11 08:55:30 +0100

    init project

---
+++ src/main/java/com/example/mytestproject/MyTestProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.mytestproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyTestProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyTestProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/mytestproject/MyTestProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mytestproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyTestProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
